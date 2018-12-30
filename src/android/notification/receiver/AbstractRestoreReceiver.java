3f1cda28152e81547906262f298098e04b08af54
Richard Mohammed <richmoh@yahoo.co.uk>
2018-12-30 09:31:10 +0000

    Applied fix from https://github.com/katzer/cordova-plugin-local-notifications/issues/1541#issuecomment-382010284

--- src/android/notification/receiver/AbstractRestoreReceiver.java
+++ src/android/notification/receiver/AbstractRestoreReceiver.java
@@ -57,8 +57,7 @@ abstract public class AbstractRestoreReceiver extends BroadcastReceiver {
     public void onReceive (Context context, Intent intent) {
         String action = intent.getAction();
 
-        if (action.equals(ACTION_BOOT_COMPLETED) && SDK_INT >= 24)
-            return;
+        if (SDK_INT >= 24) { UserManager um = (UserManager) context.getSystemService(UserManager.class); if (um == null || um.isUserUnlocked() == false) return; }
 
         Manager mgr               = Manager.getInstance(context);
         List<JSONObject> toasts = mgr.getOptions();
